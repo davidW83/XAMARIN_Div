7c0444ff2bae333e10990eaf559b778646f8fca0
David Werner <Werner@TAPNET.LOCAL>
2018-01-25 11:50:15 +0100

    Add project files.

---
+++ DependencyTest/DependencyTest/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace DependencyTest
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new DependencyTest.Tabs.TabbedPage1();
+            //MainPage = new NavigationPage( new DependencyTest.Tabs.TabbedPage1());
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
